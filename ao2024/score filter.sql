ca825817714d532dc035607fed11bcbe1eb9929d
Niftys <42993624+Niftys@users.noreply.github.com>
2024-11-05 13:49:38 -0600

    Delete ao2024 directory

--- ao2024/score filter.sql
+++
@@ -1,7 +0,0 @@
-SELECT People.name, Judges.name, Style.name, Scores.score
-FROM Scores
-JOIN Judges ON Scores.judge_id = Judges.id
-JOIN People ON Scores.people_id = People.id
-JOIN Style ON Scores.style_id = Style.id
-WHERE Judges.name LIKE '%Curtis%'
-AND People.name LIKE '%Eythan%';
